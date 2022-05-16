9880fa5a73cf4d7c641b7f7f8d71596af7ff821b
Igor J.Luc Ndiramiye <nigorjeanluc@gmail.com>
2022-05-17 01:48:48 +0200

    first and last commit

---
+++ pages/about.js
@@ -0,0 +1,19 @@
+import Head from "next/head"
+
+const About = () => {
+    return (
+      <>
+        <Head>
+          <title>Ninja List | About</title>
+          <meta name="keywords" content="ninjas" />
+        </Head>
+        <div>
+          <h1>About</h1>
+          <p>Tempor sit veniam aliqua elit. Ea id quis eu excepteur non sit magna Lorem. Ea non commodo ipsum ullamco. Est ad aliquip tempor cillum adipisicing aute enim. Consequat dolore adipisicing non in nisi dolore enim eu ea labore id sit. Consectetur do culpa officia fugiat amet sint velit cillum eu. Ut do officia sit do occaecat dolore esse amet reprehenderit pariatur aliqua.</p>
+          <p>Tempor sit veniam aliqua elit. Ea id quis eu excepteur non sit magna Lorem. Ea non commodo ipsum ullamco. Est ad aliquip tempor cillum adipisicing aute enim. Consequat dolore adipisicing non in nisi dolore enim eu ea labore id sit. Consectetur do culpa officia fugiat amet sint velit cillum eu. Ut do officia sit do occaecat dolore esse amet reprehenderit pariatur aliqua.</p>
+        </div>
+      </>
+    );
+}
+ 
+export default About;
